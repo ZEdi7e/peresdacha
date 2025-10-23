99b63f39464ac090400b84320c8b55db70ac96f3
ZEdi7e <149479906+ZEdi7e@users.noreply.github.com>
2025-10-23 15:17:19 +0300

    conftest.py

--- conftest.py
+++ conftest.py
@@ -5,8 +5,9 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    options.headless = False  # Set to True for headless mode if desired
+    options.headless = False
     driver = webdriver.Firefox(options=options)
     driver.maximize_window()
     yield driver
+
     driver.quit()
